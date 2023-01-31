99d5a20e602a5cce2d67c858cde7f3144f587a90
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-01-31 23:11:35 +0300

    feat: make Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header/Header'
 import './App.css';
 
 function App() {
-  return <div className="App">RS Clone App is started!!!</div>;
+  return <div className="App">
+    <Header />
+  </div>;
 }
 
 export default App;
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav(): JSX.Element {
+	return (
+		<nav className="nav">
+            <ul className="nav__list">
+                <li><a href="/">HOME</a></li>
+                <li><a href="/games/">GAMES</a></li>
+                <li><a href="/library/">LIBRARY</a></li>
+                <li><a href="/learn/">LEARN</a></li>
+            </ul>
+        </nav>
+	);
+}
+
+export default Nav;
