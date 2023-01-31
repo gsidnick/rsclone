91c7f2aa75398b4376569213e7ec809c1f9bd8db
gsidnick <gsidnick@gmail.com>
2023-02-01 00:34:03 +0300

    fix: remove container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Nav from '../Nav/Nav';
-import './Header.css'
+import './Header.css';
 
 function Header(): JSX.Element {
   return (
     <div className="header">
-      <div className="header__container container">
-        <a className="header__logo" href="/">LOGO</a>
+      <div className="header__container">
+        <a className="header__logo" href="/">
+          LOGO
+        </a>
         <Nav />
       </div>
     </div>
