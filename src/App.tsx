6df93cf93a2ca6d6ccf692df64ee88f5e23086c8
AlexKaroh <steam.alexkaroh@gmail.com>
2023-02-01 22:55:34 +0400

    feat: add Learn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
-// import Home from './components/Home/Home';
+import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
-import Library from './components/Library/Library';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Library />
+      <Home />
       <Footer />
     </div>
   );
