0dd03b263c56cf15e3628075866e48eef063d541
gsidnick <gsidnick@gmail.com>
2023-02-01 10:55:47 +0300

    refactor: apply format style guide

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
-import './Home.css';
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function Nav(): JSX.Element {
-	return (
-		<nav className="nav">
-            <ul className="nav__list">
-                <li><a href="/">HOME</a></li>
-                <li><a href="/games/">GAMES</a></li>
-                <li><a href="/library/">LIBRARY</a></li>
-                <li><a href="/learn/">LEARN</a></li>
-            </ul>
-        </nav>
-	);
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li>
+          <a href="/">HOME</a>
+        </li>
+        <li>
+          <a href="/games/">GAMES</a>
+        </li>
+        <li>
+          <a href="/library/">LIBRARY</a>
+        </li>
+        <li>
+          <a href="/learn/">LEARN</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
