1f7095611bf84b6a96fa0829676b72527f20100a
gsidnick <gsidnick@gmail.com>
2023-02-10 19:56:27 +0300

    refactor: add class names to nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,31 +3,38 @@ import { Link } from 'react-router-dom';
 import './Nav.css';
 
 function Nav() {
-  
   const navRef = useRef<HTMLElement>(null);
 
   const checkCurrentLink = (event: React.MouseEvent<HTMLElement>) => {
-    navRef?.current?.querySelectorAll('a').forEach(link => {
+    navRef?.current?.querySelectorAll('a').forEach((link) => {
       link.classList.remove('nav__current');
     });
 
     (event.target as HTMLElement).classList.add('nav__current');
-  }
+  };
 
   return (
     <nav ref={navRef} className="nav">
       <ul className="nav__list">
-        <li>
-          <Link onClick={checkCurrentLink} to="/">HOME</Link>
+        <li className="nav__item">
+          <Link className="nav__link" onClick={checkCurrentLink} to="/">
+            Home
+          </Link>
         </li>
-        <li>
-          <Link onClick={checkCurrentLink} to="/games/">GAMES</Link>
+        <li className="nav__item">
+          <Link className="nav__link" onClick={checkCurrentLink} to="/games/">
+            Games
+          </Link>
         </li>
-        <li>
-          <Link onClick={checkCurrentLink} to="/library/">LIBRARY</Link>
+        <li className="nav__item">
+          <Link className="nav__link" onClick={checkCurrentLink} to="/library/">
+            Library
+          </Link>
         </li>
-        <li>
-          <Link onClick={checkCurrentLink} to="/learn/">LEARN</Link>
+        <li className="nav__item">
+          <Link className="nav__link" onClick={checkCurrentLink} to="/learn/">
+            Learn
+          </Link>
         </li>
       </ul>
     </nav>
