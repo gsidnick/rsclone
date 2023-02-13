90c31af1b269999c370a47c090bce6aae37c7810
gsidnick <gsidnick@gmail.com>
2023-02-13 15:22:39 +0300

    refactor: change Home component for greeting

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,24 +1,24 @@
+import React from 'react';
+import Button from '../UI/Button/Button';
 import './Home.css';
-import React, { useContext } from 'react';
-import { MainContext } from '../../App';
 
 function Home() {
-  const { points } = useContext(MainContext);
-
   return (
     <main className="home">
       <div className="home__container container">
-        <div className="home__game">
-          <h2 className="home__game-title">Start random game</h2>
-          <button className="home__game-play">Play</button>
-        </div>
-        <div className="home__score">
-          <div className="home__card-label">Score</div>
-          <div className="home__card-value">{points} points</div>
-        </div>
-        <div className="home__level">
-          <div className="home__card-label">Level</div>
-          <div className="home__card-value">3 level</div>
+        <div className="home__greeting">
+          <h1 className="home__greeting-text">Hey, friend, let's start our journey!</h1>
+          <span className="home__greeting-description">
+            Discover new opportunities for communication, career and travel
+          </span>
+          <div className="home__group-controls">
+            <Button className="home__game-play" to="/login">
+              Log In
+            </Button>
+            <Button className="home__game-play button_outline" to="/signup">
+              Sign Up
+            </Button>
+          </div>
         </div>
       </div>
     </main>
