dddb54589879740d24e3196cc254ea96edac06b6
gsidnick <gsidnick@gmail.com>
2023-02-15 16:25:43 +0300

    refactor: change ProgressBar component styles

--- src/components/UI/ProgressBar/ProgressBar.tsx
+++ src/components/UI/ProgressBar/ProgressBar.tsx
@@ -5,8 +5,7 @@ import IProgressProps from '../../../interfaces/IProgressProps';
 function ProgressBar({ value = 0 }: IProgressProps) {
   return (
     <div className="progress">
-      {/*<span style={{ width: (library.length > 0 ? ((currentIndex + 1) * 100) / library.length : 0) + '%' }}></span>*/}
-      <span className="progress_bar" style={{ width: `${value}%` }}></span>
+      <span className="progress__bar" style={{ width: `${value}%` }}></span>
     </div>
   );
 }
