ee6a7636e5426649125af390fd483f0eb188b3b9
gsidnick <gsidnick@gmail.com>
2023-02-15 14:10:44 +0300

    feat: add WordStore

---
+++ src/store/WordStore.ts
@@ -0,0 +1,87 @@
+import { makeAutoObservable } from 'mobx';
+import WordService from '../services/WordService';
+import IWord from '../interfaces/IWord';
+
+const wordService = new WordService();
+
+class WordStore {
+  public isLoad: boolean = true;
+  public words: IWord[] = [] as IWord[];
+  public currentWord: IWord = {} as IWord;
+  private index: number = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  public setIsLoad(bool: boolean) {
+    this.isLoad = bool;
+  }
+
+  public setWords(words: IWord[]) {
+    this.words = words;
+    this.currentWord = this.words[this.index];
+  }
+
+  private setWord(word: IWord) {
+    this.words.push(word);
+  }
+
+  private unsetWord(id: string) {
+    this.words = this.words.filter((item) => id !== item._id);
+  }
+
+  public prevWord() {
+    const index = this.index - 1;
+    if (index !== -1) {
+      this.currentWord = this.words[index];
+      this.index = index;
+    }
+  }
+
+  public nextWord() {
+    const index = this.index + 1;
+    if (this.words[index] !== undefined) {
+      this.currentWord = this.words[index];
+      this.index = index;
+    }
+  }
+
+  public resetWord() {
+    this.index = 0;
+    this.currentWord = this.words[this.index];
+  }
+
+  public async getAllWords(): Promise<void> {
+    try {
+      this.setIsLoad(true);
+      const response = await wordService.getAllWords();
+      this.setWords(response.data);
+      this.setIsLoad(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  public async addWord(word: string): Promise<void> {
+    try {
+      word = word.toLowerCase();
+      const duplicate = this.words.find((item) => word === item.word);
+      if (duplicate) return;
+      const response = await wordService.addWord(word);
+      this.setWord(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  public async removeWord(word: string): Promise<void> {
+    try {
+      const response = await wordService.removeWord(word);
+      this.unsetWord(response.data._id);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+export default WordStore;
