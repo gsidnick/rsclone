32d49dc021c8ce6533393353392e7fa6ded04215
gsidnick <gsidnick@gmail.com>
2023-02-16 16:09:17 +0300

    fix: replace AppContext

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AuthStore from './store/AuthStore';
 import WordStore from './store/WordStore';
-import IAppContext from './interfaces/IAppContext';
+import { AppContext } from './context/AppContext';
 
 const authStore = new AuthStore();
 const wordStore = new WordStore();
-
-export const AppContext = React.createContext<IAppContext>({} as IAppContext);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
