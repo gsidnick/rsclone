8d43c3530648b8404da564596ad11ca519619e32
gsidnick <gsidnick@gmail.com>
2023-02-19 01:28:34 +0300

    feat: add Loader to Library page

--- src/components/Library/Library.tsx
+++ src/components/Library/Library.tsx
@@ -4,6 +4,7 @@ import { observer } from 'mobx-react-lite';
 import Input from '../UI/Input/Input';
 import Button from '../UI/Button/Button';
 import useStores from '../../hooks/useStores';
+import Loader from '../UI/Loader/Loader';
 
 function Library() {
   const { wordStore } = useStores();
@@ -29,26 +30,31 @@ function Library() {
   return (
     <main className="library">
       <div className="library__container container">
-        <div className="library__group-controls">
-          <Input
-            name="word"
-            type="text"
-            placeholder="Type new word here..."
-            value={word}
-            onChange={(e) => setWord(e.target.value)}
-          />
-          <Button className="" onClick={() => wordStore.addWord(word)}>
-            Add
-          </Button>
-        </div>
-        <div className="library__list">
-          <div className="library__row-head">
-            <div className="library__col">Word</div>
-            <div className="library__col">Translation</div>
-            <div className="library__col">Learn</div>
-          </div>
-          <div className="library__row-body">{renderRows()}</div>
-        </div>
+        {wordStore.isLoading && <Loader />}
+        {!wordStore.isLoading && (
+          <>
+            <div className="library__group-controls">
+              <Input
+                name="word"
+                type="text"
+                placeholder="Type new word here..."
+                value={word}
+                onChange={(e) => setWord(e.target.value)}
+              />
+              <Button className="" onClick={() => wordStore.addWord(word)}>
+                Add
+              </Button>
+            </div>
+            <div className="library__list">
+              <div className="library__row-head">
+                <div className="library__col">Word</div>
+                <div className="library__col">Translation</div>
+                <div className="library__col">Learn</div>
+              </div>
+              <div className="library__row-body">{renderRows()}</div>
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
