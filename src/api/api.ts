ae0a2b59c87bc1b8af7994142ee8404c512a4ff3
gsidnick <gsidnick@gmail.com>
2023-02-19 00:32:06 +0300

    refactor: change access token name

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,7 +7,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
   return config;
 });
 
