0eca295040441c3438469eb5da70d534d6772a4b
gsidnick <gsidnick@gmail.com>
2023-02-19 19:56:33 +0300

    feat: implement Modal to AuthLogin

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,28 +1,44 @@
+import './Home.css';
 import React from 'react';
 import Button from '../UI/Button/Button';
-import './Home.css';
+import useStores from '../../hooks/useStores';
+import { observer } from 'mobx-react-lite';
+import ModalAuthLogin from '../Modal/ModalAuthLogin/ModalAuthLogin';
 
 function Home() {
+  const { modalStore } = useStores();
+
+  function openLoginForm() {
+    modalStore.openModal();
+  }
+
+  function openSignupForm() {
+    modalStore.openModal();
+  }
+
   return (
-    <main className="home">
-      <div className="home__container container">
-        <div className="home__greeting">
-          <h1 className="home__greeting-text">Hey, friend, let's start our journey!</h1>
-          <span className="home__greeting-description">
-            Discover new opportunities for communication, career and travel
-          </span>
-          <div className="home__group-controls">
-            <Button className="home__game-play" to="/login">
-              Log In
-            </Button>
-            <Button className="home__game-play button_outline" to="/signup">
-              Sign Up
-            </Button>
+    <>
+      {modalStore.isModal && <ModalAuthLogin />}
+      <main className="home">
+        <div className="home__container container">
+          <div className="home__greeting">
+            <h1 className="home__greeting-text">Hey, friend, let's start our journey!</h1>
+            <span className="home__greeting-description">
+              Discover new opportunities for communication, career and travel
+            </span>
+            <div className="home__group-controls">
+              <Button className="home__game-play" onClick={openLoginForm}>
+                Log In
+              </Button>
+              <Button className="home__game-play button_outline" onClick={openSignupForm}>
+                Sign Up
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
-export default Home;
+export default observer(Home);
