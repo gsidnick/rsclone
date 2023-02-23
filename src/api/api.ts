52c2b350e43a1da72df30f6a8d7e4e9221fe1771
gsidnick <gsidnick@gmail.com>
2023-02-23 18:08:25 +0300

    refactor: use token service

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
+import TokenService from '../services/TokenService';
 
 const SERVER_URL = process.env.REACT_APP_RAILWAY_URL;
 const api = axios.create({
   withCredentials: true,
   baseURL: `${SERVER_URL}/api`,
 });
+const tokenService = new TokenService();
 
 api.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+  config.headers.Authorization = `Bearer ${tokenService.getAccessToken()}`;
   return config;
 });
 
