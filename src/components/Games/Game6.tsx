90a6c645a0f02766bd7d5e0f82ce90c55958e59f
gsidnick <gsidnick@gmail.com>
2023-02-26 01:13:23 +0300

    feat: implement save points in Game #6

--- src/components/Games/Game6.tsx
+++ src/components/Games/Game6.tsx
@@ -1,71 +1,89 @@
 import './Game.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import Button from '../UI/Button/Button';
 import Loader from '../UI/Loader/Loader';
 import useStores from '../../hooks/useStores';
 import WordIteratorStore from '../../store/WordIteratorStore';
 import WordListenStore from '../../store/WordListenStore';
-import EarSvg from '../../images/ear.svg';
 import { useTranslation } from 'react-i18next';
+import GameEndMessage from '../Messages/GameEndMessage';
+import EarSvg from '../../images/ear.svg';
 
 const wordIteratorStore = new WordIteratorStore();
 const wordListenStore = new WordListenStore();
 
 function Game6() {
   const { t } = useTranslation();
+  const { wordStore, gameStore, modalStore } = useStores();
 
-  const { wordStore, gameStore } = useStores();
-  wordIteratorStore.setWords(wordStore.words);
-  wordListenStore.setWords(wordStore.words);
-
-  function checkYes() {
-    let flag = wordListenStore.isCorrect;
-    if (flag === true) {
+  function yesButtonHandler() {
+    if (wordListenStore.isCorrect) {
       gameStore.setCorrect();
-      gameStore.setIncrementPoints();
       wordIteratorStore.nextWord();
     } else {
       gameStore.setWrong();
-      gameStore.setDecrementPoints();
       wordIteratorStore.nextWord();
     }
   }
 
-  function checkNo() {
-    let flag = wordListenStore.isCorrect;
-    if (flag === false) {
+  function noButtonHandler() {
+    if (!wordListenStore.isCorrect) {
       gameStore.setCorrect();
-      gameStore.setIncrementPoints();
       wordIteratorStore.nextWord();
     } else {
       gameStore.setWrong();
-      gameStore.setDecrementPoints();
       wordIteratorStore.nextWord();
     }
+  }
+
+  function sayButtonHandler() {
+    wordListenStore.sayWord();
   }
 
   function skipButtonHandler() {
+    gameStore.setWrong();
     wordIteratorStore.nextWord();
   }
+
+  useEffect(() => {
+    if (!wordStore.isLoading) {
+      wordIteratorStore.setWords(wordStore.words);
+      wordListenStore.setWords(wordStore.words);
+      wordListenStore.setQuestion(wordIteratorStore.current);
+      gameStore.setTotal(wordStore.words.length);
+      gameStore.iterator = wordIteratorStore;
+    }
+  }, [wordStore.isLoading]);
+
+  useEffect(() => {
+    if (!wordStore.isLoading && wordIteratorStore.current !== undefined) {
+      wordListenStore.setWords(wordStore.words);
+      wordListenStore.setQuestion(wordIteratorStore.current);
+    }
+  }, [wordIteratorStore.current]);
+
+  useEffect(() => {
+    if (wordIteratorStore.isEnd) {
+      modalStore.openModal(<GameEndMessage />);
+    }
+  }, [wordIteratorStore.isEnd]);
 
   return (
     <main className="game">
       {wordStore.isLoading && <Loader />}
       {!wordStore.isLoading && (
         <>
-          {wordListenStore.setQuestion(wordIteratorStore.current)}
-          {wordListenStore.randomWord()}
           <span className="game__word-label">{t('It translates as')}</span>
-          <h2 className="game__word">{wordListenStore.question?.word}</h2>
+          <h2 className="game__word">{wordIteratorStore.current.word}</h2>
           <div className="game__group-controls">
-            <img src={EarSvg} onClick={() => wordListenStore.wiretap()} className="game__img-hear" alt="Ear" />
+            <img src={EarSvg} onClick={sayButtonHandler} className="game__img-hear" alt="Ear" />
           </div>
           <div className="game__group-controls">
-            <Button onClick={checkYes}>
+            <Button onClick={yesButtonHandler}>
               <>{t('Yes')}</>
             </Button>
-            <Button className="button_red" onClick={checkNo}>
+            <Button className="button_red" onClick={noButtonHandler}>
               <>{t('No')}</>
             </Button>
           </div>
