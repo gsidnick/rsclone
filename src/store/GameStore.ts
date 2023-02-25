6514cdc2fbc2836b6784ef8e655919cf0b00f343
gsidnick <gsidnick@gmail.com>
2023-02-25 14:21:28 +0300

    refactor: change setWrong method

--- src/store/GameStore.ts
+++ src/store/GameStore.ts
@@ -38,8 +38,6 @@ class GameStore {
 
   public setWrong() {
     this.wrong = this.wrong + 1;
-    if (this.points === 0) return;
-    this.points = this.points - 1;
   }
 
   public setTotal(total: number) {
