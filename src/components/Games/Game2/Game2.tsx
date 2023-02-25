fcc06f1058ad618bc63bf0ab165e956e52b74b96
gsidnick <gsidnick@gmail.com>
2023-02-25 18:35:20 +0300

    feat: implement save points in Game #2

--- src/components/Games/Game2/Game2.tsx
+++ src/components/Games/Game2/Game2.tsx
@@ -1,48 +1,54 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react';
+import './Game2.css';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
+import Button from '../../UI/Button/Button';
 import Loader from '../../UI/Loader/Loader';
 import useStores from '../../../hooks/useStores';
 import WordIteratorStore from '../../../store/WordIteratorStore';
 import WordCharsStore from '../../../store/WordCharsStore';
-import './Game2.css';
-import Button from '../../UI/Button/Button';
+import GameEndMessage from '../../Messages/GameEndMessage';
 
 const wordIteratorStore = new WordIteratorStore();
 const wordCharsStore = new WordCharsStore();
 
 function Game2() {
-  const { wordStore, gameStore } = useStores();
+  const { wordStore, gameStore, modalStore } = useStores();
 
   useEffect(() => {
-    if (wordStore.isLoading === false) {
+    if (!wordStore.isLoading) {
       wordIteratorStore.setWords(wordStore.words);
+      gameStore.setTotal(wordStore.words.length);
+      gameStore.iterator = wordIteratorStore;
       wordCharsStore.setWord(wordIteratorStore.current.translation);
       wordCharsStore.setShuffleWord();
     }
   }, [wordStore.isLoading]);
 
   useEffect(() => {
-    if (wordCharsStore.isCorrect === true) {
-      gameStore.setCorrect();
-      gameStore.setIncrementPoints();
-      wordIteratorStore.nextWord();
-    }
-    if (wordCharsStore.notIsCorrect === true) {
-      gameStore.setWrong();
-      gameStore.setDecrementPoints();
-      wordIteratorStore.nextWord();
-    }
-  }, [wordCharsStore.isCorrect, wordCharsStore.notIsCorrect]);
-
-  useEffect(() => {
-    if (wordIteratorStore.current !== undefined && wordStore.isLoading === false) {
+    if (wordIteratorStore.current !== undefined && !wordStore.isLoading) {
       wordCharsStore.setWord(wordIteratorStore.current.translation);
       wordCharsStore.setShuffleWord();
       wordCharsStore.changeIsCorrectToFalse();
       wordCharsStore.changeNotIsCorrectToFalse();
     }
   }, [wordIteratorStore.current]);
+
+  useEffect(() => {
+    if (wordIteratorStore.isEnd) {
+      modalStore.openModal(<GameEndMessage />);
+    }
+  }, [wordIteratorStore.isEnd]);
+
+  useEffect(() => {
+    if (wordCharsStore.isCorrect) {
+      gameStore.setCorrect();
+      wordIteratorStore.nextWord();
+    }
+    if (wordCharsStore.notIsCorrect) {
+      gameStore.setWrong();
+      wordIteratorStore.nextWord();
+    }
+  }, [wordCharsStore.isCorrect, wordCharsStore.notIsCorrect]);
 
   return (
     <div className="game">
