5d60602c0bcfcf30fbc011e1f91d24cd5c7edbca
gsidnick <gsidnick@gmail.com>
2023-02-25 15:09:21 +0300

    feat: add level breakpoints & updateLevel method

--- src/store/StatisticStore.ts
+++ src/store/StatisticStore.ts
@@ -7,6 +7,7 @@ class StatisticStore {
   public isLoading: boolean = true;
   public score: number = 0;
   public level: number = 1;
+  private breakpoints = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
   constructor() {
     makeAutoObservable(this);
@@ -18,18 +19,30 @@ class StatisticStore {
 
   public setScore(score: number) {
     this.score = score;
+    this.updateLevel();
   }
 
   private setLevel(level: number) {
     this.level = level;
   }
 
+  private updateLevel() {
+    let level: number = 1;
+    this.breakpoints.forEach((bp, i) => {
+      if (this.score >= bp) level = i + 2;
+    });
+    if (this.level < level) {
+      this.setLevel(level);
+      this.updateStatistic();
+    }
+  }
+
   public async fetchStatistic(): Promise<void> {
     try {
       this.setIsLoading(true);
       const response = await statisticService.getStatistic();
-      this.setScore(response.data.score);
       this.setLevel(response.data.level);
+      this.setScore(response.data.score);
       this.setIsLoading(false);
     } catch (error) {
       console.error(error);
